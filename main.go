03d30862878252f919a1a81b67ae3b8681c891dd
lilendian0x00 <Joshuacol115@proton.me>
2023-08-12 23:30:05 +0100

    Removed unnecessary codes

--- main.go
+++ main.go
@@ -4,22 +4,6 @@ import (
 	"xray-knife/cmd"
 )
 
-func init() {
-	//common.Must(common.RegisterConfig((*proxyman.InboundConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
-	//	return inbound.New(ctx, config.(*proxyman.InboundConfig)), nil
-	//}))
-	//common.Must(common.RegisterConfig((*core.InboundHandlerConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
-	//	return inbound.NewHandler(ctx, config.(*core.InboundHandlerConfig)), nil
-	//}))
-
-	//common.Must(common.RegisterConfig((*proxyman.OutboundConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
-	//	return outbound.New(ctx, config.(*proxyman.OutboundConfig)), nil
-	//}))
-	//common.Must(common.RegisterConfig((*core.OutboundHandlerConfig)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
-	//	return outbound.NewHandler(ctx, config.(*core.OutboundHandlerConfig)), nil
-	//}))
-}
-
 func main() {
 	cmd.Execute()
 }
