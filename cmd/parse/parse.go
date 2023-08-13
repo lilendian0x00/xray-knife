3815b85166dfcca324b346ce858e5c1b0bfa0c6f
lilendian0x00 <Joshuacol115@proton.me>
2023-08-13 03:22:34 +0100

    Improved parsing argument

--- cmd/parse/parse.go
+++ cmd/parse/parse.go
@@ -3,40 +3,68 @@ package parse
 import (
 	"bufio"
 	"fmt"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"os"
+	"time"
+	"xray-knife/utils"
 	"xray-knife/xray"
 )
 
 var (
-	configLink string
+	readFromSTDIN   bool
+	configLink      string
+	configLinksFile string
 )
 
 // ParseCmd represents the parse command
 var ParseCmd = &cobra.Command{
 	Use:   "parse",
 	Short: "Gives a detailed info about the config link",
-	Long:  `Default reads from STDIN, you can use '-c' flags to pass xray config from arguments`,
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		if configLink == "" {
+		if len(args) < 1 && readFromSTDIN != true && configLink == "" && configLinksFile == "" {
+			cmd.Help()
+			return
+		}
+		if readFromSTDIN {
 			reader := bufio.NewReader(os.Stdin)
 			fmt.Println("Reading config from STDIN:")
 			text, _ := reader.ReadString('\n')
 			configLink = text
 
+		} else if configLinksFile != "" {
+			links := utils.ParseFileByNewline(configLinksFile)
+			//fmt.Println(links)
+			d := color.New(color.FgCyan, color.Bold)
+			for i, link := range links {
+				d.Printf("Config Number: %d\n", i+1)
+				protocol, err := xray.ParseXrayConfig(link)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%v", err)
+					os.Exit(1)
+				}
+				fmt.Println(protocol.DetailsStr() + "\n")
+				time.Sleep(time.Duration(25) * time.Millisecond)
+			}
+			return
+
 		}
 
-		fmt.Printf("\n")
-
-		protocol, err := xray.ParseXrayConfig(configLink)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v", err)
-			os.Exit(1)
+		if readFromSTDIN || configLink != "" {
+			fmt.Printf("\n")
+			protocol, err := xray.ParseXrayConfig(configLink)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v", err)
+				os.Exit(1)
+			}
+			fmt.Println(protocol.DetailsStr())
 		}
-		fmt.Println(protocol.DetailsStr())
 	},
 }
 
 func init() {
-	ParseCmd.Flags().StringVarP(&configLink, "config", "c", "", "The xray config link")
+	ParseCmd.Flags().BoolVarP(&readFromSTDIN, "stdin", "i", false, "Read config link from STDIN")
+	ParseCmd.Flags().StringVarP(&configLink, "config", "c", "", "The config link")
+	ParseCmd.Flags().StringVarP(&configLinksFile, "file", "f", "", "Read config links from a file")
 }
