fa1a7a8e134baefda2c2321d67fa5ccd359ab28a
lilendian0x00 <Joshuacol115@proton.me>
2023-08-13 03:22:13 +0100

    Added a function to read file based on newline char

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,11 @@
 package utils
 
 import (
+	"bufio"
 	"encoding/base64"
 	"os"
 	"strings"
+	"xray-knife/utils/customlog"
 )
 
 func Base64Decode(b64 string) ([]byte, error) {
@@ -18,6 +20,33 @@ func Base64Decode(b64 string) ([]byte, error) {
 		return base64.URLEncoding.DecodeString(b64)
 	}
 	return b, nil
+}
+
+func ParseFileByNewline(fileName string) []string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		customlog.Printf(customlog.Failure, "Error in reading file: %v\n", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// Set the Scanner to split on newline characters
+	scanner.Split(bufio.ScanLines)
+
+	var lines []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		lines = append(lines, line)
+	}
+
+	if scanner.Err() != nil {
+		customlog.Printf(customlog.Failure, "Error in parsing file: %v\n", scanner.Err())
+	}
+	return lines
 }
 
 func WriteIntoFile(fileName string, data []byte) error {
