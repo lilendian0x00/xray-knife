782381362ef04532f6a267bdf1b433a1bb043bcb
lilendian0x00 <Joshuacol115@proton.me>
2023-08-15 16:30:20 +0100

    Added ICMP (PING) test

--- cmd/net/icmp.go
+++ cmd/net/icmp.go
@@ -3,18 +3,59 @@ package net
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"net"
+	"os"
+	"xray-knife/network"
+	"xray-knife/utils/customlog"
+	"xray-knife/xray"
+)
+
+var (
+	destIP    net.IP
+	testCount uint16
 )
 
 // IcmpCmd represents the icmp command
 var IcmpCmd = &cobra.Command{
 	Use:   "icmp",
-	Short: "Send icmp packet",
+	Short: "PING or ICMP test config's host",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("icmp called")
+		parsed, err := xray.ParseXrayConfig(configLink)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+			os.Exit(1)
+		}
+
+		generalDetails, err := parsed.ConvertToGeneralConfig()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+			os.Exit(1)
+		}
+
+		destIP = net.ParseIP(generalDetails.Address)
+		if destIP != nil {
+			addr, err := net.LookupIP(generalDetails.Address)
+			if err != nil {
+				customlog.Printf(customlog.Failure, "Error when doing reverse lookup: %v", err)
+				os.Exit(1)
+			}
+			destIP = addr[0]
+		}
+
+		icmp := network.IcmpPacket{
+			DestIP:    destIP,
+			TestCount: testCount,
+		}
+		err = icmp.MeasureReplyDelay()
+		if err != nil {
+			customlog.Printf(customlog.Failure, "MeasureReplyDelay Error: %v", err)
+			os.Exit(1)
+		}
 	},
 }
 
 func init() {
-
+	IcmpCmd.Flags().StringVarP(&configLink, "config", "c", "", "The xray config link")
+	IcmpCmd.Flags().Uint16VarP(&testCount, "count", "t", 4, "Count of tests")
 }
---
+++ network/icmp.go
@@ -0,0 +1,118 @@
+package network
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"math/rand"
+	"net"
+	"os"
+	"time"
+	"xray-knife/utils/customlog"
+)
+
+type IcmpPacket struct {
+	DestIP                 net.IP
+	CustomInternetProtoID  int
+	CustomSequenceNum      int
+	Data                   []byte
+	TestCount              uint16
+	DelayBetweenEachPacket uint16
+}
+
+func (i *IcmpPacket) MeasureReplyDelay() error {
+	rnd := rand.New(rand.NewSource(time.Now().Unix()))
+
+	if i.DestIP == nil {
+		customlog.Printf(customlog.Failure, "Destination IP address is empty!\n")
+		return errors.New("Destination IP address is empty! ")
+	}
+	if i.CustomInternetProtoID == 0 {
+		i.CustomInternetProtoID = os.Getpid() & 0xffff
+	}
+	if i.CustomSequenceNum == 0 {
+		i.CustomSequenceNum = rnd.Intn(4294967290)
+	}
+	if len(i.Data) == 0 {
+		// Windows default DATA
+		data := []byte("abcdefghijklmnopqrstuvwabcdefghi")
+		buf := bytes.NewReader(data)
+		i.Data = make([]byte, 32)
+		err := binary.Read(buf, binary.LittleEndian, i.Data)
+		if err != nil {
+			customlog.Printf(customlog.Failure, "binary.Read failed: %v\n", err)
+			return err
+		}
+	}
+	c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+	if err != nil {
+		customlog.Printf(customlog.Failure, "listen err: %v\n", err)
+		return err
+	}
+	defer c.Close()
+
+	wm := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID: i.CustomInternetProtoID, Seq: i.CustomSequenceNum,
+			Data: i.Data,
+		},
+	}
+	wb, err := wm.Marshal(nil)
+	if err != nil {
+		return err
+	}
+	// Record the current time
+	start := time.Now()
+
+	msg := make(chan int)
+	errMsg := make(chan string)
+	//done := make(chan bool, 1)
+	go func() {
+		for a := 0; a < 5; a++ {
+			_, err = c.WriteTo(wb, &net.IPAddr{IP: i.DestIP})
+			start = time.Now()
+			if err != nil {
+				msg <- -1
+				errMsg <- fmt.Sprintf("WriteTo err: %s", err.Error())
+			} else {
+				msg <- 0
+			}
+			time.Sleep(time.Duration(1) * time.Second)
+		}
+		msg <- 1
+	}()
+
+	for m := range msg {
+		if m == 0 {
+			rb := make([]byte, 1500)
+			n, peer, err1 := c.ReadFrom(rb)
+			if err1 != nil {
+				customlog.Printf(customlog.Failure, "Error ReadFrom: %v\n", err1)
+				return err
+			}
+			rm, err2 := icmp.ParseMessage(ipv4.ICMPTypeEchoReply.Protocol(), rb[:n])
+			if err2 != nil {
+				customlog.Printf(customlog.Failure, "Error icmp.ParseMessage: %v\n", err2)
+				return err
+			}
+			switch rm.Type {
+			case ipv4.ICMPTypeEchoReply:
+				customlog.Printf(customlog.Success, "Reply from %s: bytes=32 time=%dms\n", peer, time.Since(start).Milliseconds())
+				break
+			default:
+				customlog.Printf(customlog.Failure, "Got %+v\n", rm)
+			}
+		} else if m == -1 {
+			// Error happened
+			sendErr := <-errMsg
+			customlog.Printf(customlog.Failure, "%s\n", sendErr)
+		} else {
+			break
+		}
+	}
+	return nil
+}
