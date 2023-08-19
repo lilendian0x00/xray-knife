02494e0cf3dfe5660f74a0d27117d9438981c3d1
lilendian0x00 <Joshuacol115@proton.me>
2023-08-20 00:52:57 +0100

    Minor change

--- xray/shadowsocks_test.go
+++ xray/shadowsocks_test.go
@@ -3,7 +3,7 @@ package xray
 import "testing"
 
 func TestShadowSocks_Parse(t *testing.T) {
-	var ss ShadowSocks
+	var ss Shadowsocks
 	err := ss.Parse("ss://YWVzLTI1Ni1nY206RXhhbXBsZUAxMjM0@example.com:443#exa")
 	if err != nil {
 		t.Errorf("Error when parsing: %v", err)
