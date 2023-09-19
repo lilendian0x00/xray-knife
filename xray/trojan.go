a3bc4f3df1fdeac991bf16aaa233c546af2d5857
lilendian0x00 <Joshuacol115@proton.me>
2023-09-19 21:22:10 +0100

    Fixed TCP obfus conn

--- xray/trojan.go
+++ xray/trojan.go
@@ -168,7 +168,7 @@ func (t *Trojan) BuildOutboundDetourConfig(allowInsecure bool) (*conf.OutboundDe
 					"path": %s,
 					"headers": {
 						"Host": %s,
-						"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
+						"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
 					}
 				}
 			}
--- xray/vless.go
+++ xray/vless.go
@@ -185,7 +185,7 @@ func (v *Vless) BuildOutboundDetourConfig(allowInsecure bool) (*conf.OutboundDet
 					"path": %s,
 					"headers": {
 						"Host": %s,
-						"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
+						"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
 					}
 				}
 			}
