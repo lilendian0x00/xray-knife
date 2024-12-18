588250b7291fb432e37da631c39458d7104c5029
lilendian0x00 <116685464+lilendian0x00@users.noreply.github.com>
2024-12-17 23:55:51 -0800

    Fixed singbox

--- internal/singbox/singbox_test.go
+++ internal/singbox/singbox_test.go
@@ -3,6 +3,7 @@ package singbox
 import (
 	"io"
 	"testing"
+	"time"
 )
 
 func TestHysteria2_MakeHttpClient(t *testing.T) {
@@ -20,7 +21,7 @@ func TestHysteria2_MakeHttpClient(t *testing.T) {
 		return
 	}
 
-	client, _, err := s.MakeHttpClient(protocol)
+	client, _, err := s.MakeHttpClient(protocol, time.Duration(10)*time.Second)
 	if err != nil {
 		t.Errorf(err.Error())
 		return
@@ -55,13 +56,48 @@ func TestVless_MakeHttpClient(t *testing.T) {
 		return
 	}
 
-	client, _, err := s.MakeHttpClient(protocol)
+	client, _, err := s.MakeHttpClient(protocol, time.Duration(10)*time.Second)
 	if err != nil {
 		t.Errorf(err.Error())
 		return
 	}
 
 	resp, err := client.Get("http://httpbin.org/ip")
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	all, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	resp.Body.Close()
+
+	t.Logf("%s\n", string(all))
+}
+
+func TestWireguard_MakeHttpClient(t *testing.T) {
+	s := NewSingboxService(false, false)
+
+	config := "wireguard://WJD7jPqCgI%2BXxujP3d%2FaqzUOJUjjWvlFIoHnK0AQGmk%3D@188.114.97.225:5279?address=172.16.0.2%2F32%2C2606%3A4700%3A110%3A8f81%3Ad551%3Aa0%3A532e%3Aa2b3%2F128&reserved=98%2C233%2C215&publickey=bmXOC%2BF1FxEMF9dyiK2H5%2F1SUtzH0JuVo51h2wPfgyo%3D&mtu=1280&keepalive=5&wnoise=quic&wnoisecount=15&wnoisedelay=1&wpayloadsize=5-10#HELI 2024-12-18 10:02"
+	protocol, err := s.CreateProtocol(config)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	err = protocol.Parse()
+	if err != nil {
+		return
+	}
+
+	client, _, err := s.MakeHttpClient(protocol, time.Duration(10)*time.Second)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	resp, err := client.Get("http://httpbin.org")
 	if err != nil {
 		t.Errorf(err.Error())
 		return
