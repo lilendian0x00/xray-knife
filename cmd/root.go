29b3452909ab26025ebbf83356f6826a380ce5a4
lilendian0x00 <116685464+lilendian0x00@users.noreply.github.com>
2024-12-19 12:45:20 -0800

    Updated version

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,7 @@ var rootCmd = &cobra.Command{
 	Use:     "github.com/lilendian0x00/xray-knife",
 	Short:   "Swiss Army Knife for xray-core & sing-box",
 	Long:    ``,
-	Version: "3.16.30",
+	Version: "3.16.32",
 	// Main Tools:
 	//1. parse: Parses xray config link.
 	//2. net: Multiple network tests for xray configs.
