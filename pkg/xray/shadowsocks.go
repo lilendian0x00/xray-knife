3f4a736ffeefccb3cc17597640684b711fe9f4f3
lilendian0x00 <116685464+lilendian0x00@users.noreply.github.com>
2025-05-26 03:40:12 -0700

    Turned off uot

--- pkg/xray/shadowsocks.go
+++ pkg/xray/shadowsocks.go
@@ -127,7 +127,7 @@ func (s *Shadowsocks) BuildOutboundDetourConfig(allowInsecure bool) (*conf.Outbo
 		"port": %v,
 		"password": "%s",
 		"method": "%s",
-		"uot": true
+		"uot": false
 	}
   ]
 }`, s.Address, s.Port, s.Password, s.Encryption)))
