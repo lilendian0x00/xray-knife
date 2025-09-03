50d894118c45f9c3c8820414a4c08e43dfa07d23
lilendian0x00 <116685464+lilendian0x00@users.noreply.github.com>
2025-09-03 08:22:38 +0100

    Updated version

--- cmd/root.go
+++ cmd/root.go
@@ -20,7 +20,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "xray-knife",
 	Short:   "Swiss Army Knife for xray-core & sing-box",
-	Version: "7.1.1",
+	Version: "7.3.2",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
